3b2657895c74fca0ab1a202eeb1c8447c904cc72
zachang <zachangdawuda@gmail.com>
2018-08-22 17:55:41 +0100

    create ride implementation

--- app/models/ride.py
+++ app/models/ride.py
@@ -11,19 +11,19 @@ class Ride(Base):
 
     user_id = db.Column(db.String, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     car_name = db.Column(db.String(100), nullable=True)
-    departure_time = db.Column(db.String(250), nullable=False)
+    departure_time = db.Column(db.DateTime)
     seat_count = db.Column(db.Integer, nullable=False)
     seat_taken = db.Column(db.Integer, default=0, nullable=False)
-    available = db.Column(db.Boolean, default=False, nullable=True)
+    available = db.Column(db.Boolean, default=False, nullable=False)
+    completed = db.Column(db.String(10), default='False', nullable=False)
     rates = db.relationship('Rate', backref='ride', lazy=True)
 
 
     def __init__(self, **kwargs):
         """initialize class."""
-        self.user_d = kwargs['user_d']
         self.departure_time = kwargs['departure_time']
         self.seat_count = kwargs['seat_count']
-
+        self.car_name = kwargs['car_name']
 
     def __repr__(self):
         return "<Ride: {}>".format(self.__tablename__)
--- migrations/versions/51478c19457b_.py
+++ migrations/versions/51478c19457b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 3b5d33149dd8
+Revision ID: 51478c19457b
 Revises: 
-Create Date: 2018-08-17 23:35:22.133234
+Create Date: 2018-08-22 14:15:33.476045
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3b5d33149dd8'
+revision = '51478c19457b'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -43,10 +43,11 @@ def upgrade():
     sa.Column('updated_at', sa.DateTime(), nullable=True),
     sa.Column('user_id', sa.String(), nullable=False),
     sa.Column('car_name', sa.String(length=100), nullable=True),
-    sa.Column('departure_time', sa.String(length=250), nullable=False),
+    sa.Column('departure_time', sa.DateTime(), nullable=True),
     sa.Column('seat_count', sa.Integer(), nullable=False),
     sa.Column('seat_taken', sa.Integer(), nullable=False),
-    sa.Column('available', sa.Boolean(), nullable=True),
+    sa.Column('available', sa.Boolean(), nullable=False),
+    sa.Column('completed', sa.String(length=10), nullable=False),
     sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('id')
