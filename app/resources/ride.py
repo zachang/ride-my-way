de99f787cbec8d167e39bafdd4212269ba997627
zachang <zachangdawuda@gmail.com>
2018-08-23 11:31:17 +0100

    feature implementation that enables users delete their created ride

--- app/resources/ride.py
+++ app/resources/ride.py
@@ -21,20 +21,21 @@ class Rides(Resource):
         errors = ride_schema.validate(payload)
         current_user = get_jwt_identity()
         valid_user = User.get_one(current_user)
-        restrict_ride = valid_user.rides
-
-        for ride in restrict_ride:
-            if ride.completed == 'False' and ride.available:
-                return response_builder({
-                    'status': 'fail',
-                    'message': 'You have an available ride and can not create another'
-                    }, 403)
        
         if not valid_user:
             return response_builder({
                 'status': 'fail',
                 'message': 'User not found'
                 }, 404)
+
+
+        restrict_ride = valid_user.rides
+        for ride in restrict_ride:
+            if ride.completed == 'False' and ride.available:
+                return response_builder({
+                    'status': 'fail',
+                    'message': 'You have an available ride and can not create another'
+                    }, 403)
 
         if payload:
             departure = payload['departure_time']
@@ -87,13 +88,12 @@ class Rides(Resource):
 
 
 class UserRides(Resource):
-    """Resource class to retrieve rides created by a user"""
+    """Resource class to retrieve, delete and update rides created by a user"""
 
     @jwt_required
     def get(self, user_id):
         current_user = get_jwt_identity()
         valid_user = User.get_one(user_id)
-        user_rides = valid_user.rides
         
         if not valid_user:
             return response_builder({
@@ -107,11 +107,49 @@ class UserRides(Resource):
                 'message': 'You can only view rides you created'
                 }, 403)
 
+        user_rides = valid_user.rides
         if user_rides:
             return response_builder({
                 'status': 'success',
                 'rides': rides_schema.dump(user_rides).data
                 })
+        else:
+            return response_builder({
+                'status': 'success',
+                'message': 'You have not created any ride yet'
+                })
+
+
+    @jwt_required
+    def delete(self, user_id):
+        current_user = get_jwt_identity()
+        valid_user = User.get_one(user_id)
+
+
+        if not valid_user:
+            return response_builder({
+                'status': 'fail',
+                'message': 'User not found'
+                }, 404)
+
+        if valid_user.id != current_user:
+            return response_builder({
+                'status': 'fail',
+                'message': 'You can only update rides you created'
+                }, 403)
+
+        user_rides = valid_user.rides
+        if user_rides:
+            if user_rides[0].completed in ['False', 'cancelled']:
+                user_rides[0].delete()
+                return response_builder({
+                    'status': 'success',
+                    'message': 'Ride deleted'
+                    })
+            return response_builder({
+                    'status': 'fail',
+                    'message': 'You can not delete a completed Ride'
+                    }, 403)
         else:
             return response_builder({
                 'status': 'success',
