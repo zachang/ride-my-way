7adc1cf290844d602e7b3c403886c7f38b77394f
zachang <zachangdawuda@gmail.com>
2018-08-24 12:18:26 +0100

    fix bug when updating user ride

--- app/resources/ride.py
+++ app/resources/ride.py
@@ -7,6 +7,7 @@ from app.models.user import User
 from app.schemas.ride import RideSchema, RideSchemaEdit
 from app.utils.response_builder import response_builder
 from app.utils.to_lower_strip import to_lower_strip
+from app.utils.verify_ride_id import verify_ride_decorator
 
 rides_schema = RideSchema(many=True)
 ride_schema = RideSchema()
@@ -177,15 +178,30 @@ class UserSingleRide(Resource):
     """Resource class to update a ride created by a user"""
 
     @jwt_required
+    @verify_ride_decorator(Ride)
     def put(self, ride_id):
         payload = request.get_json(silent=True)
-        valid_user = User.get_one(get_jwt_identity())
         errors = ride_schema_edit.validate(payload)
         
         if errors:
             return errors, 400
 
         if payload:
+            current_user = get_jwt_identity()
+            valid_user = User.get_one(payload.get('user_id'))
+
+            if not valid_user:
+                return response_builder({
+                    'status': 'fail',
+                    'message': 'User not found'
+                    }, 404)
+
+            if valid_user.id != current_user:
+                return response_builder({
+                    'status': 'fail',
+                    'message': 'You can only edit rides you created'
+                    }, 403)
+
             user_rides = valid_user.rides
             if user_rides:
                 ride_to_update = []
@@ -193,18 +209,18 @@ class UserSingleRide(Resource):
                     if user_ride.id == ride_id:
                         ride_to_update.append(user_ride)
                         break
-
+                    
                 if payload.get('departure_time'):
-                    departure = payload['departure_time']
+                    departure_time = payload.get('departure_time')
                     _format = '%Y-%m-%d %H:%M:%S'
 
-                    if datetime.datetime.strptime(departure, _format) < datetime.datetime.utcnow():
+                    if datetime.datetime.strptime(departure_time, _format) < datetime.datetime.utcnow():
                         return response_builder({
                             'status': 'fail',
-                            'message': 'Your departure time can not be less than the current time'
+                            'message': 'Your departure date/time must be at least current'
                             }, 403)
                     else:
-                        ride_to_update[0].departure_time = payload.get('departure_time')
+                        ride_to_update[0].departure_time = departure_time
 
                 if payload.get('car_name'):
                     ride_to_update[0].car_name = to_lower_strip(payload.get('car_name'))
